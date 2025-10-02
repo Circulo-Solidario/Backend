098577251b3ec9618d9df406c6736d3782d6780d
405865-OntiveroDavid <405865@tecnicatura.frc.utn.edu.ar>
2025-10-02 08:58:39 -0300

    Updated/Changes producto y categoria
    - Se cambio las categorias para que se generen en la bd y en el producto este enlazado a la categoria por el id
    - Se paso los estados de un producto a un enum
    - Se realizacion configuraciones nuevas y cambios en algunas

--- src/main/java/com/backend/Backend/dtos/CategoriaDTO.java
+++ src/main/java/com/backend/Backend/dtos/CategoriaDTO.java
@@ -1,5 +1,6 @@
 package com.backend.Backend.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CategoriaDTO {
     private Long id;
-    private String categoria;
+    private String nombre;
+    private String descripcion;
 }
