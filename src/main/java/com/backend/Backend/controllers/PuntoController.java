f3682330f0da5656345ec41f7ccd8edece275528
405865-OntiveroDavid <405865@tecnicatura.frc.utn.edu.ar>
2025-11-12 21:30:51 -0300

    Fix and added put estado
    - Se arreglo que diera un error al querer crear un punto causado por el id
    - Se añadio el put para que se pueda actualizar el estado de un punto

--- src/main/java/com/backend/Backend/controllers/PuntoController.java
+++ src/main/java/com/backend/Backend/controllers/PuntoController.java
@@ -1,10 +1,14 @@
 package com.backend.Backend.controllers;
 
+import com.backend.Backend.dtos.punto.EstadoRequest;
+import com.backend.Backend.dtos.punto.PuntoRequest;
 import com.backend.Backend.models.Punto;
 import com.backend.Backend.repositories.PuntoRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/puntos")
@@ -23,8 +27,28 @@ public class PuntoController {
     }
 
     @PostMapping
-    public Punto crear(@RequestBody Punto punto) {
-        punto.setEstado("pendiente");
+    public Punto crear(@RequestBody PuntoRequest dto) {
+        Punto punto = Punto.builder()
+                .latitud(dto.getLatitud())
+                .longitud(dto.getLongitud())
+                .descripcion(dto.getDescripcion())
+                .estado("pendiente")
+                .build();
+
         return repo.save(punto);
+    }
+
+    @PutMapping("/{id}/estado")
+    public ResponseEntity<Punto> actualizarEstado(
+            @PathVariable Long id,
+            @RequestBody EstadoRequest request) {
+
+        Punto punto = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Punto no encontrado"));
+
+        punto.setEstado(request.getEstado());
+        repo.save(punto);
+
+        return ResponseEntity.ok(punto);
     }
 }
